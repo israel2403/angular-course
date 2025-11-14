6235ee3653448231f91dad9508c814dd961a0a8a
Israel Huerta Fer <israelhf24@gmail.com>
2025-11-14 07:14:09 -0600

    Building our first angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @NgModule({
   declarations: [
@@ -10,8 +11,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
-  ],
+    BrowserAnimationsModule,
+    CourseCardComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
